0327d51bea2bb3bb5d1331a20fc4573fe5d0e3d6
MoazElsewedy <92794214+MoazElsewedy@users.noreply.github.com>
2021-10-26 00:22:15 +0200

    (Supplier) first push in the program
    this program only has the  supplier design for the moment

---
+++ Cafe Pos/Cafe Pos/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace Cafe_Pos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Top_panel = new System.Windows.Forms.Panel();
+            this.search_txtbox = new System.Windows.Forms.TextBox();
+            this.Nav_panel = new System.Windows.Forms.Panel();
+            this.suppier1 = new Cafe_Pos.Suppier();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Sup_but = new System.Windows.Forms.Button();
+            this.Home_but = new System.Windows.Forms.Button();
+            this.search_but = new System.Windows.Forms.Button();
+            this.ChooseTable_but = new System.Windows.Forms.Button();
+            this.Top_panel.SuspendLayout();
+            this.Nav_panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Top_panel
+            // 
+            this.Top_panel.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Top_panel.Controls.Add(this.ChooseTable_but);
+            this.Top_panel.Controls.Add(this.search_but);
+            this.Top_panel.Controls.Add(this.search_txtbox);
+            this.Top_panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Top_panel.Location = new System.Drawing.Point(0, 0);
+            this.Top_panel.Name = "Top_panel";
+            this.Top_panel.Size = new System.Drawing.Size(1902, 100);
+            this.Top_panel.TabIndex = 1;
+            // 
+            // search_txtbox
+            // 
+            this.search_txtbox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.search_txtbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.search_txtbox.Font = new System.Drawing.Font("Microsoft Tai Le", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.search_txtbox.Location = new System.Drawing.Point(200, 45);
+            this.search_txtbox.Name = "search_txtbox";
+            this.search_txtbox.Size = new System.Drawing.Size(406, 49);
+            this.search_txtbox.TabIndex = 0;
+            // 
+            // Nav_panel
+            // 
+            this.Nav_panel.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Nav_panel.Controls.Add(this.button6);
+            this.Nav_panel.Controls.Add(this.button5);
+            this.Nav_panel.Controls.Add(this.button4);
+            this.Nav_panel.Controls.Add(this.button1);
+            this.Nav_panel.Controls.Add(this.button3);
+            this.Nav_panel.Controls.Add(this.button2);
+            this.Nav_panel.Controls.Add(this.Sup_but);
+            this.Nav_panel.Controls.Add(this.Home_but);
+            this.Nav_panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Nav_panel.Location = new System.Drawing.Point(0, 100);
+            this.Nav_panel.Name = "Nav_panel";
+            this.Nav_panel.Size = new System.Drawing.Size(200, 933);
+            this.Nav_panel.TabIndex = 2;
+            // 
+            // suppier1
+            // 
+            this.suppier1.BackColor = System.Drawing.Color.White;
+            this.suppier1.Location = new System.Drawing.Point(200, 100);
+            this.suppier1.Name = "suppier1";
+            this.suppier1.Size = new System.Drawing.Size(1702, 933);
+            this.suppier1.TabIndex = 3;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button6.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(158)))));
+            this.button6.Image = global::Cafe_Pos.Properties.Resources.pie_chart_2_;
+            this.button6.Location = new System.Drawing.Point(28, 698);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(136, 108);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Reports";
+            this.button6.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(158)))));
+            this.button5.Image = global::Cafe_Pos.Properties.Resources.setting;
+            this.button5.Location = new System.Drawing.Point(28, 813);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(136, 108);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Settings";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(158)))));
+            this.button4.Image = global::Cafe_Pos.Properties.Resources.shopping_bag__2_;
+            this.button4.Location = new System.Drawing.Point(28, 594);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(136, 108);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Order";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(158)))));
+            this.button1.Image = global::Cafe_Pos.Properties.Resources.dollar;
+            this.button1.Location = new System.Drawing.Point(28, 480);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 108);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Casheir";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(158)))));
+            this.button3.Image = global::Cafe_Pos.Properties.Resources.table__1_;
+            this.button3.Location = new System.Drawing.Point(28, 356);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 108);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Tables";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(158)))));
+            this.button2.Image = global::Cafe_Pos.Properties.Resources.user;
+            this.button2.Location = new System.Drawing.Point(28, 242);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 108);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Customer";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // Sup_but
+            // 
+            this.Sup_but.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Sup_but.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.Sup_but.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Sup_but.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Sup_but.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(158)))));
+            this.Sup_but.Image = global::Cafe_Pos.Properties.Resources.supplier;
+            this.Sup_but.Location = new System.Drawing.Point(9, 120);
+            this.Sup_but.Name = "Sup_but";
+            this.Sup_but.Size = new System.Drawing.Size(182, 116);
+            this.Sup_but.TabIndex = 1;
+            this.Sup_but.Text = "Supplier";
+            this.Sup_but.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Sup_but.UseVisualStyleBackColor = false;
+            this.Sup_but.Click += new System.EventHandler(this.Sup_but_Click_1);
+            // 
+            // Home_but
+            // 
+            this.Home_but.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Home_but.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.Home_but.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Home_but.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Home_but.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(158)))));
+            this.Home_but.Image = global::Cafe_Pos.Properties.Resources.home__1_;
+            this.Home_but.Location = new System.Drawing.Point(28, 6);
+            this.Home_but.Name = "Home_but";
+            this.Home_but.Size = new System.Drawing.Size(136, 108);
+            this.Home_but.TabIndex = 0;
+            this.Home_but.Text = "Home";
+            this.Home_but.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Home_but.UseVisualStyleBackColor = false;
+            // 
+            // search_but
+            // 
+            this.search_but.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.search_but.FlatAppearance.BorderSize = 0;
+            this.search_but.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.search_but.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.search_but.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.search_but.Image = global::Cafe_Pos.Properties.Resources.search;
+            this.search_but.Location = new System.Drawing.Point(612, 56);
+            this.search_but.Name = "search_but";
+            this.search_but.Size = new System.Drawing.Size(37, 38);
+            this.search_but.TabIndex = 1;
+            this.search_but.UseVisualStyleBackColor = true;
+            // 
+            // ChooseTable_but
+            // 
+            this.ChooseTable_but.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(128)))), ((int)(((byte)(25)))));
+            this.ChooseTable_but.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.ChooseTable_but.FlatAppearance.BorderSize = 0;
+            this.ChooseTable_but.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(128)))), ((int)(((byte)(25)))));
+            this.ChooseTable_but.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(128)))), ((int)(((byte)(25)))));
+            this.ChooseTable_but.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ChooseTable_but.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChooseTable_but.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.ChooseTable_but.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ChooseTable_but.Location = new System.Drawing.Point(1638, 46);
+            this.ChooseTable_but.Name = "ChooseTable_but";
+            this.ChooseTable_but.Size = new System.Drawing.Size(252, 49);
+            this.ChooseTable_but.TabIndex = 18;
+            this.ChooseTable_but.Text = "Select Table";
+            this.ChooseTable_but.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.Controls.Add(this.suppier1);
+            this.Controls.Add(this.Nav_panel);
+            this.Controls.Add(this.Top_panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Top_panel.ResumeLayout(false);
+            this.Top_panel.PerformLayout();
+            this.Nav_panel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Top_panel;
+        private System.Windows.Forms.Button search_but;
+        private System.Windows.Forms.TextBox search_txtbox;
+        private System.Windows.Forms.Panel Nav_panel;
+        private System.Windows.Forms.Button Home_but;
+        private Suppier suppier1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Sup_but;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button ChooseTable_but;
+    }
+}
+
---
+++ Cafe Pos/Cafe Pos/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cafe_Pos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            suppier1.Hide();
+        }
+
+       
+        private void Sup_but_Click_1(object sender, EventArgs e)
+        {
+            suppier1.Show();
+        }
+    }
+}
